1de259bddcd04bd801d13191a2f2da21ff61de20
lepm11 <lepm11@hotmail.com>
2022-05-24 14:56:35 -0500

    Se sube el Gatewayserver

---
+++ GatewayServer/src/main/java/com/ibm/academia/gateway/filters/ProjectFilter.java
@@ -0,0 +1,7 @@
+package com.ibm.academia.gateway.filters;
+
+import org.springframework.stereotype.Component;
+@Component
+public class ProjectFilter implements org.springframework.cloud.gateway.filter.GlobalFilter{
+
+}
